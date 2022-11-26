5f6e89b43aa9426608cd67ee5fd265637182d9d1
Sumi <qkobr94@gmail.com>
2022-11-26 18:04:01 +0900

    create navigator and add scss styling

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -1,5 +1,44 @@
+import "./Navigator.scss";
+
 const Navigator = (): JSX.Element => {
-  return <div>Navigator</div>;
+  return (
+    <div className="navigator">
+      <div className="navigator__column">
+        <div className="navigator__box">
+          <img src="../../img/img_portrait.jpg" alt="#" />
+        </div>
+        <div className="navigator__box">Sumi</div>
+        <div className="navigator__box">equepem@gmail.com</div>
+      </div>
+      <div className="navigator__column">
+        <div className="navigator__box">
+          <li>
+            <a href="#"> Dashboard</a>
+          </li>
+        </div>
+        <div className="navigator__box">
+          <li>
+            <a href="#">Expenses</a>
+          </li>
+        </div>
+        <div className="navigator__box">
+          <li>
+            <a href="#">Plan</a>
+          </li>
+        </div>
+        <div className="navigator__box">
+          <li>
+            <a href="#"> Summary</a>
+          </li>
+        </div>
+        <div className="navigator__box">
+          <li>
+            <a href="#"> Settings</a>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navigator;
